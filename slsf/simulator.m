e478de07a7f0f46304d68775329a8ff73dbdf173
vasanth-mahendran <vasanth.mahendran@objectedge.com>
2017-03-14 19:10:37 -0500

    spread sheet and complex signal mismatch fix

--- slsf/simulator.m
+++ slsf/simulator.m
@@ -23,6 +23,10 @@ classdef simulator < handle
         
         function obj = simulator(generator, max_try)
             % CONSTRUCTOR %
+            filename = 'untitled.xlsx';
+            A = [1,0; 2,1; 3,0; 4,1;5,0;6,1;7,0;8,1;9,0;10,1];
+            sheet = 'Sheet1';
+            xlswrite(filename,A,sheet);
             obj.generator = generator;
             obj.max_try = max_try;
             obj.fixed_blocks = mymap();
@@ -63,7 +67,7 @@ classdef simulator < handle
                 sim(obj.generator.sys);
                 disp(['RETURN FROM SIMULATION. STATUS: ' obj.sim_status ]);
                 stop(myTimer);
-
+ 
                 delete(myTimer);
             catch e
                 throw(e);
@@ -139,7 +143,7 @@ classdef simulator < handle
                 % Get my output type
                 my_out_type = c.n.get_output_type(fxd);
                 
-
+ 
                 for i=1:numel(c.n.out_nodes)
                     for j=1:numel(c.n.out_nodes{i})
                         chld = c.n.out_nodes{i}{j};
@@ -231,11 +235,11 @@ classdef simulator < handle
                             disp(['Multiple Errors. Solving ' int2str(m_i)]);
                             ei = e.cause{m_i}
                             obj.generator.my_result.exc = ei;
-
+ 
                             ei.message
                             ei.cause
                             ei.stack
-
+ 
                             disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
                             
                             [done, ret, found] = obj.look_for_solutions(ei, true, done, ret);
@@ -246,11 +250,11 @@ classdef simulator < handle
                             end
                             
                         end
-
+ 
                     else
                         [done, ret, found] = obj.look_for_solutions(e, false, done, ret);
                     end
-
+ 
                 end
                 
                 if done && found % Don't waste executing below block if simulation fixer was not done.
@@ -270,7 +274,7 @@ classdef simulator < handle
                 
                 
             end         %       fix-and-simulate loop
-
+ 
                     
         end
         
@@ -280,14 +284,14 @@ classdef simulator < handle
             found = false;          % Did the exception matched with any of our fixers
             
             if isa(e, 'MSLException')
-
+ 
                 if util.starts_with(e.identifier, 'Simulink:Engine:AlgLoopTrouble')
                     obj.fix_alg_loop(e);
                     found = true;
                 elseif util.starts_with(e.identifier, 'Simulink:Engine:PortDimsMismatch')
                     [done, found] = obj.fix_port_dimensions_mismatch(e);
                 else
-
+ 
                     switch e.identifier
                         case {'Simulink:Engine:AlgStateNotFinite', 'Simulink:Engine:UnableToSolveAlgLoop', 'Simulink:Engine:BlkInAlgLoopErr'}
                             obj.fix_alg_loop(e);
@@ -335,12 +339,20 @@ classdef simulator < handle
                         case {'Simulink:Engine:SolverConsecutiveZCNum'}
                             done = obj.fix_solver_consecutive_zc(e);
                             found = true;
+                        
+                        case {'Simulink:DataType:InputPortComplexSignalMismatch'}
+                            done = obj.fix_complex_signal_mismatch(e,'both');
+                            found = true;
+                        
+                        case {'sl_iofile:excelfile:BlockSimError'}
+                            done = obj.fix_spread_sheet(e);
+                            found = true;
                             
                         otherwise
                             done = true;
                     end
                 end
-
+ 
             else
                 done = true;                                        % TODO
             end
@@ -455,9 +467,9 @@ classdef simulator < handle
             
             for i = 1:numel(e.handles)
                 inner = e.handles{i};
-
+ 
                 h = util.select_me_or_parent(inner);
-
+ 
 %                 if at_output
                 switch loc
                     case {'output'}
@@ -492,7 +504,7 @@ classdef simulator < handle
                         
             for i = 1:numel(e.handles)
                 inner = e.handles{i};
-
+ 
                 h = util.select_me_or_parent(inner);
                 obj.add_block_in_the_middle(h, sprintf('simulink/Discrete/Zero-Order\nHold'), false, true);
             end
@@ -503,7 +515,7 @@ classdef simulator < handle
         function obj = fix_alg_loop(obj, e)
             % Fix Algebraic Loop 
 %             handles = e.handles{1}
-
+ 
 %             handles(1)
 %             handles(2)
 %             
@@ -544,7 +556,7 @@ classdef simulator < handle
             ret = mycell(-1);
             
             my_name = get_param(h, 'Name');
-
+ 
             disp(['Add Block in the middle: For ' my_name '; handle ' num2str(h)]);
             
             if ignore_in
@@ -554,20 +566,20 @@ classdef simulator < handle
             if ignore_out
                 disp('IGNORE OUTPUTS');
             end
-
+ 
             try
                 ports = get_param(h,'PortConnectivity');
             catch e
                 disp('~ Skipping, not a block');
                 return;
             end
-
+ 
             for j = 1:numel(ports)
                 p = ports(j);
                 is_inp = [];
                 
                 % Detect if current port is Input or Output
-
+ 
                 if isempty(p.SrcBlock) || p.SrcBlock == -1
                     is_inp = false;
                 end
@@ -621,19 +633,19 @@ classdef simulator < handle
                     ret.add(d_h);
                     return;
                 end
-
+ 
                 disp(['Const. ' dir ' ' other_name ' ; port ' num2str(other_port) '; My type ' p.Type ]);
-
+ 
                 other_b_p = [other_name '/' num2str(other_port)];
                 
-
+ 
                 % get a new block
-
+ 
                 [d_name, d_h] = obj.generator.add_new_block(replacement);
                 ret.add(d_h);
-
+ 
                 %  delete and Connect
-
+ 
                 new_blk_port = [d_name '/1'];
                 
                 if is_inp
@@ -650,9 +662,9 @@ classdef simulator < handle
                 add_line(obj.generator.sys, new_blk_port, b_b , 'autorouting','on');
                 
                 disp('Done adding block!');
-
+ 
                
-
+ 
             end
             
             
@@ -663,11 +675,11 @@ classdef simulator < handle
         function d_h = add_block_in_middle_multi(obj,my_b_p, o_names, o_ports, replacement)
             
             % get a new block
-
+ 
             [d_name, d_h] = obj.generator.add_new_block(replacement);
-
+ 
             %  delete and Connect
-
+ 
             new_blk_port = [d_name '/1'];
             add_line(obj.generator.sys, my_b_p, new_blk_port , 'autorouting','on');
                         
@@ -705,22 +717,22 @@ classdef simulator < handle
                     fprintf('No Algebraic loop. Returning...\n');
                     return;
                 end
-
+ 
                 for i = 1:numel(aloops)
                     cur_loop = aloops(i);
-
+ 
                     visited_handles = mycell(-1);
-
+ 
                     for j = 1:numel(cur_loop.VariableBlockHandles)
                         j_block = cur_loop.VariableBlockHandles(1);
                         effective_j_blk = util.select_me_or_parent(j_block);
-
+ 
                         fprintf('j blk: %s \t effective blk: %s\n',get_param(j_block, 'name'), get_param(effective_j_blk, 'name'));
-
+ 
                         if util.cell_in(visited_handles.data, effective_j_blk)
                             fprintf('Blk already visited\n');
                         else
-
+ 
                             visited_handles.add(effective_j_blk);
                             
                             fprintf('[AlgLoopEliminator] Adding new block....\n');
@@ -737,9 +749,64 @@ classdef simulator < handle
             end
         end
         
+        function done = fix_complex_signal_mismatch(obj, e, loc, blk_params)
+            
+            if nargin < 4
+                blk_params = []; % Parameters for the new block
+            end
+            
+            
+            disp('FIXING DATA TYPE MISMATCH...');
+            done = false;
+            
+%             if ~isempty(obj.last_handle) && strcmp(obj.generator.last_exc.identifier, e.identifier)
+%                 disp('Same error as last one. Check for handle...');
+%                 if obj.last_handle == 
+%             end
+            
+            for i = 1:numel(e.handles)
+                inner = e.handles{i};
+ 
+                h = util.select_me_or_parent(inner);
+ 
+%                 if at_output
+                switch loc
+                    case {'output'}
+                        new_blocks = obj.add_block_in_the_middle(h, 'Simulink/Math Operations/Complex to Real-Imag', true, false);
+                        break;
+                    case {'input'}
+                        new_blocks = obj.add_block_in_the_middle(h, 'Simulink/Math Operations/Complex to Real-Imag', false, true);
+                        break;
+                    case {'both'}
+                        new_blocks = obj.add_block_in_the_middle(h, 'Simulink/Math Operations/Complex to Real-Imag', true, false);
+                        more_new = obj.add_block_in_the_middle(h, 'Simulink/Math Operations/Complex to Real-Imag', false, true);
+                        new_blocks.extend(more_new);
+                        break;
+                    otherwise
+                        throw(MException('RandGen:FixDataType:InvalidValForParamLOC', 'Invalid value for parameter loc'));
+                end
+            end
+            
+            if ~isempty(blk_params) 
+                for i=1:new_blocks.len
+                    for j=1:numel(blk_params)
+                        set_param(new_blocks.get(i), blk_params{j}{1}, blk_params{j}{2});
+                    end
+                end
+            end
+                 
+        end
         
+        function done = fix_spread_sheet(obj, e)
+            done = false;
+            for j = 1:numel(e.handles)
+                handles = e.handles{j};
+                set_param(handles, 'SheetName', 'untitled');
+            end  
+        end
         
     end
     
 end
+ 
 
